bc89ce8da7fe500b403536085784f78f4f6e8896
duongnva-2181 <nguyen.van.anh.duong@sun-asterisk.com>
2021-02-19 13:57:26 +0700

    init view and livewire

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @livewire('contact-component')
+            </div>
+        </div>
+    </div>
+@endsection
